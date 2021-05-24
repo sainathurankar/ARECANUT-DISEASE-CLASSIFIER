7766cd64e3647aac5643533ee302347cd5e7df57
sainathurankar <37251166+sainathurankar@users.noreply.github.com>
2021-05-25 01:41:42 +0530

    language update

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function init() {
     dz.on("complete", function (file) {
         let imageData = file.dataURL;
 
-        var url = "http://3e4626734497.ngrok.io/classify_image";
+        var url = "https://dc3cfdbe0ee3.ngrok.io/classify_image";
 
         $.post(url, {
             image_data: file.dataURL
